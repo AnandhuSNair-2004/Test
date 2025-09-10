583836e455a7726f6b19ade33ebe92c9abd5367c
AnandhuSNair-2004 <anandhusnair26112004@gmail.com>
2025-09-10 15:41:09 +0530

    Create calculate_salary.py
    Calculating the salary of an employ.

---
+++ Python/calculate_salary.py
@@ -0,0 +1,8 @@
+basic_pay = float(input("Enter the basic pay: "))
+HRA = basic_pay * 0.10
+ta = basic_pay * 0.05
+total_salary = basic_pay + HRA + ta
+print("Basic pay: ", basic_pay)
+print("HRA pay: ", HRA)
+print("ta pay: ", ta)
+print("Total salary: ", total_salary)
