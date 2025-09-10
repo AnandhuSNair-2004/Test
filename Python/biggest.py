b6412f1328ec21f1397a759a6a6fb3cdb464210f
AnandhuSNair-2004 <anandhusnair26112004@gmail.com>
2025-09-10 15:39:56 +0530

    Create biggest.py
    Find the biggest number

---
+++ Python/biggest.py
@@ -0,0 +1,4 @@
+a = float(input("Enter the first number: "))
+b = float(input("Enter the second number: "))
+c = float(input("Enter the third number: "))
+print(f"The biggest number is" ,max(a, b, c))
